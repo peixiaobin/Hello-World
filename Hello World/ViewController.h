f53f0b63bfa7596ab17e8555e298f0a9c505b59f
peixiaobin <chanjetmacbook@MacBook-Pro.local>
2013-02-27 13:39:09 +0800

    Initial Commit

---
+++ Hello World/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Chanjet MAC book on 13-2-27.
+//  Copyright (c) 2013年 Chanjet MAC book. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Hello World/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Hello World
+//
+//  Created by Chanjet MAC book on 13-2-27.
+//  Copyright (c) 2013年 Chanjet MAC book. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
